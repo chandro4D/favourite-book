63e0d3b51ddf8cbe224ecfb2cf68bf1004958663
chandro4D <chandroshakhor7@gmail.com>
2024-03-26 13:33:18 +0600

    add navbar

---
+++ src/Components/ListedBooks/ListedBooks.jsx
@@ -0,0 +1,9 @@
+const ListedBooks = () => {
+    return (
+        <div>
+            <h3>This Is Listed Books Section</h3>
+        </div>
+    );
+};
+
+export default ListedBooks;
---
+++ src/Components/PagesToRead/PagesToRead.jsx
@@ -0,0 +1,11 @@
+
+
+const PagesToRead = () => {
+    return (
+        <div>
+            <h3>This Is Pages to Read Section</h3>
+        </div>
+    );
+};
+
+export default PagesToRead;
