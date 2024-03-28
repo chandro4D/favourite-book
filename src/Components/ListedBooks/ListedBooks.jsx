6ed9e4349eb5c67b1452fee6c46ff11cc2b33103
chandro4D <chandroshakhor7@gmail.com>
2024-03-28 22:09:42 +0600

    add listed books

--- src/Components/ListedBooks/ListedBooks.jsx
+++ src/Components/ListedBooks/ListedBooks.jsx
@@ -1,7 +1,12 @@
 const ListedBooks = () => {
     return (
         <div>
-            <h3>This Is Listed Books Section</h3>
+            <div className="mt-10 bg-slate-300 text-center ml-[135px] pt-6 w-[1180px] h-[100px] text-[#131313] text-3xl font-bold">
+                <h3>Books</h3>
+            </div>
+            <div className="w-[145px] h-[52px] text-center rounded-lg pt-3 mt-[32px] ml-[650px] bg-[#23BE0A] text-lg font-semibold text-[#FFFFFF]">
+                <button>Sort By</button>
+            </div>
         </div>
     );
 };
